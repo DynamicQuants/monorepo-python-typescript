cb2e0cd3dec6f78bcb9158b8faabdb2347fde6e5
Carlos Cosming <carlos@kingly.cl>
2024-11-06 16:42:42 -0300

    ✨ feat: add nestjs configs

--- configs/jest-config/nestjs.ts
+++ configs/jest-config/nestjs.ts
@@ -1,14 +1,16 @@
 import type { Config } from 'jest';
 import { config as baseConfig } from './base';
 
-export const config = {
+const config = {
   ...baseConfig,
-  rootDir: 'src',
-  testRegex: '.*\\.spec\\.ts$',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testEnvironment: 'node',
+  testRegex: '.spec.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
-  testEnvironment: 'node',
 } as const satisfies Config;
+
+export default config;
